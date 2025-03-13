1423d660d1840f1ce328919a1e5618c7a54815e4
rithanyars20 <rithanyars20@gmail.com>
2025-03-13 04:48:40 -0400

    Add files via upload
    2

--- mood_music_app.py
+++ mood_music_app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 from mood_music import get_playlist
 
-st.title("🎵 AI-Powered Mood-based Music Playlist Generator")
+st.title("🎵 MoodPlay: an AI-Powered Mood-based Music Playlist Generator")
 
 # Mood selection
 mood_options = ["Happy", "Chill", "Romantic", "Sad", "Energetic"]
-selected_mood = st.radio("Select a Mood:", mood_options, horizontal=True)
+selected_mood = st.radio("How are you feeling today?", mood_options, horizontal=True)
 
 if st.button("Generate Playlist"):
     playlist = get_playlist(selected_mood.lower(), n=100)
